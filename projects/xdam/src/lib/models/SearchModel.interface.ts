ec71d43f8c49d1ff389ea5ebe252d98c914174d6
Alejandro de Tovar <venespana159@gmail.com>
2019-06-07 13:26:41 +0200

    Added new interfaces

--- projects/xdam/src/lib/models/SearchModel.interface.ts
+++ projects/xdam/src/lib/models/SearchModel.interface.ts
@@ -2,3 +2,13 @@ export interface SearchModel {
     limit?: number;
     page?: number;
 }
+
+export interface SearchOptions {
+    input?: SearchInputOptions;
+}
+
+export interface SearchInputOptions {
+    hasSearch?: boolean;
+    hasReset?: boolean;
+    hasClear?: boolean;
+}
