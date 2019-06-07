c2fc39181f895eb77f9c1f0cc35acf212b3e0a99
Alejandro de Tovar <venespana159@gmail.com>
2019-06-07 14:50:36 +0200

    Changed config loader

--- src/app/mappers/RouterMapper.ts
+++ src/app/mappers/RouterMapper.ts
@@ -2,8 +2,8 @@ import { hasIn, isNil } from 'ramda';
 import { HttpParams } from '@angular/common/http';
 import { isFunction } from 'util';
 
-import { Settings } from '../../../projects/xdam/src/lib/models/Settings.interface';
-import { ItemModel } from '../../../projects/xdam/src/lib/models/ItemModel.interface';
+import { Settings } from '../../../projects/xdam/src/lib/models/interfaces/Settings.interface';
+import { ItemModel } from '../../../projects/xdam/src/lib/models/interfaces/ItemModel.interface';
 
 /**
  * Mapper class for routes and models configurations in index or environment file
