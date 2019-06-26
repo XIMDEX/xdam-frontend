7ae2dc8c7119d1f16dff762ab721c9c40eeafcfb
Alejandro de Tovar <venespana159@gmail.com>
2019-06-26 17:57:10 +0200

    Added file component to questions

---
+++ projects/xdam/src/lib/components/forms/questions/file/file.component.ts
@@ -0,0 +1,28 @@
+import { QuestionBaseComponent } from './../question.component.base';
+import { Component, Input } from '@angular/core';
+import { File as QuestionFile } from './../../../../models/forms/questions/File';
+
+@Component({
+    selector: 'xdam-file',
+    templateUrl: './file.component.html',
+    styleUrls: ['./file.component.scss']
+})
+export class FileComponent extends QuestionBaseComponent<FileList | any[]> {
+    @Input() question: QuestionFile;
+
+    protected defaultValue = [];
+
+    get accept(): string {
+        return this.question.accept;
+    }
+
+    get multiple(): boolean {
+        return this.question.multiple;
+    }
+
+    protected changeValue(evt: Event = null) {
+        const { target } = evt;
+        let files: FileList | File = (target as HTMLInputElement).files;
+        return this.onChange.emit(files);
+    }
+}
