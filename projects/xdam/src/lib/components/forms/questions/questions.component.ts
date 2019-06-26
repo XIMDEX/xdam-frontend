a16117dcc9f97ea3b112b1e8f4bb66c6a4ed8ba2
Alejandro de Tovar <venespana159@gmail.com>
2019-06-26 16:58:08 +0200

    Created basic question components

---
+++ projects/xdam/src/lib/components/forms/questions/questions.component.ts
@@ -0,0 +1,21 @@
+import { QuestionBaseComponent } from './question.component.base';
+import { Component, Input, HostBinding } from '@angular/core';
+import { Base as QuestionBase } from './../../../models/forms/questions/Base';
+import { Text as QuestionText } from '../../../models/forms/questions/Text';
+
+@Component({
+    selector: 'xdam-question',
+    templateUrl: './questions.component.html',
+    styleUrls: ['./questions.component.scss']
+})
+export class QuestionsComponent extends QuestionBaseComponent<any> {
+    @Input() question: QuestionBase<any> | QuestionText;
+
+    @HostBinding('class.field-item') baseClass = true;
+
+    protected defaultValue: any = null;
+
+    protected changeValue(value: any = null) {
+        return this.onChange.emit(value);
+    }
+}
