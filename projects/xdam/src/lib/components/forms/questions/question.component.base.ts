4a233c5d3de42847bf85aaf50354e95923dfbf84
Alejandro de Tovar <venespana159@gmail.com>
2019-06-26 16:58:08 +0200

    Created basic question components

---
+++ projects/xdam/src/lib/components/forms/questions/question.component.base.ts
@@ -0,0 +1,40 @@
+import { isNil, hasIn } from 'ramda';
+import { Input, Output, EventEmitter } from '@angular/core';
+
+export abstract class QuestionBaseComponent<T> {
+    @Input() question: any;
+    @Output() onChange = new EventEmitter<T>();
+
+    protected _value: T;
+    protected abstract defaultValue: T;
+
+    get key(): string {
+        return this.question.key;
+    }
+
+    get label(): string | null {
+        let label = null;
+        if (hasIn('label', this.question) && !isNil(this.question.label)) {
+            label = this.question.label;
+        }
+        return label;
+    }
+
+    set value(value: T) {
+        this._value = value;
+    }
+
+    get value(): T {
+        let value: T = this.defaultValue;
+        if (isNil(this._value) && !isNil(this.question.value)) {
+            value = this.question.value;
+        } else if (!isNil(this._value)) {
+            value = this._value;
+        }
+        return value;
+    }
+
+    protected changeValue() {
+        return this.onChange.emit(this.value);
+    }
+}
