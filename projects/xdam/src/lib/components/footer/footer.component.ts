ce30c318ab1a01af0c843844976b584d566ef9da
Alejandro de Tovar <venespana159@gmail.com>
2019-07-10 14:40:06 +0200

    Updated version and display footer data

--- projects/xdam/src/lib/components/footer/footer.component.ts
+++ projects/xdam/src/lib/components/footer/footer.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { version, homepage, author } from '../../../../package.json';
 
 @Component({
     selector: 'xdam-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+    public version: string = version;
+    public homepage: string = homepage;
+    public author: { name: string; url: string } = author;
+}
